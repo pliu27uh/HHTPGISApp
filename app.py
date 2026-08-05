c02b679e8cc4d8f5772e979ca25557d25cb19b0d
pliu27uh <pliu27@cougarnet.uh.edu>
2026-08-05 10:14:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from streamlit_folium import st_folium
 # ==========================================================
 
 st.set_page_config(
-    page_title="National Hydrogen Transportation Fuel Supply",
+    page_title="National Hydrogen Transportation Fuel Supply (alpha)",
     layout="wide",
     initial_sidebar_state="collapsed"  # Collapses the sidebar on initial page load
 )
